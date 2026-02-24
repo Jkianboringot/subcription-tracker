3dc4678c307ab872321a2ffc20fde29da63e57b4
jkian <slyrics443@gmail.com>
2026-02-24 15:28:30 +0800

    added arject

--- app.js
+++ app.js
@@ -11,17 +11,21 @@ import errorMiddleware from './middlewares/error.middleware.js'
 
 import condb from './database/mongodb.js'
 import cookieParser from 'cookie-parser'
+import arcjetMiddleware from './middlewares/arcjet.middleware.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use(cookieParser())
+app.use(arcjetMiddleware)
 
 
 app.use('/api/v1/auth',authRouter)
 app.use('/api/v1/users',userRoute)
 app.use('/api/v1/subs',subRoute)
+
+
 
 app.use(errorMiddleware)
 
